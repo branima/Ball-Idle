06fce014a4d6b814cc5b241237fcd381b71eae8e
branima <thebranima@gmail.com>
2022-09-19 11:11:28 +0200

    Ready for GP build

--- Ball Idle/Assets/LevelLoader.cs
+++ Ball Idle/Assets/LevelLoader.cs
@@ -7,6 +7,7 @@ public class LevelLoader : MonoBehaviour
 {
     void Awake()
     {
+        //TTPCore.Setup(); ///Uncomment this line when the clik is integrated
         SaveData saveData = SaveSystem.LoadGame();
         int levelIdx = saveData != null ? saveData.level : 1;
         SceneManager.LoadScene(levelIdx);
